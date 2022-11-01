24f15a4fe6943432312c2e619ae4d3d7f59de8d6
ashikur rahman <ashik.cse.hstu@gmail.com>
2022-11-01 23:19:36 +0600

    solving ongoing

---
+++ Online Judges Code/Codeforces/Codeforces Contest/831 div1+2/A_Factorise_N_M.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int tc;
+    cin >> tc;
+    while (tc--)
+    {
+        int n;
+        cin >> n;
+        if (n == 2)
+        {
+            cout << "7\n";
+        }
+        else
+        {
+            cout << "3\n";
+        }
+    }
+}
