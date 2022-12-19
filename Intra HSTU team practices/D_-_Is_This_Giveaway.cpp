db68bcf82bf09a81aefa373ecd014dc0e3405a87
ashikur rahman <ashik.cse.hstu@gmail.com>
2022-12-19 20:32:57 +0600

    adding cf

---
+++ Intra HSTU team practices/D_-_Is_This_Giveaway.cpp
@@ -0,0 +1,151 @@
+/**        بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ (In the name of God, the Most Gracious, the Most Merciful.)       */
+/*
+      Institution : Hajee Mohammad Danesh Science and Technology University,Dinajpur,Bangladesh.
+      Name        : Ashikur Rahman Bhuyain (Asif)
+      Email       : ashik.cse.hstu@gmail.com
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+
+#define FasterIO             \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0)
+#define TIME clock() * 1.0 / CLOCKS_PER_SEC
+#define pi acos(-1.0)
+#define mem(a, b) memset(a, b, sizeof(a))
+#define all(a) a.begin(), a.end()
+#define Sort(x) sort(x.begin(), x.end())
+#define Reverse(x) reverse(x.begin(), x.end())
+#define SortA(ar, s) sort(ar, ar + s)
+#define SortD(ar, s) sort(ar, ar + s, greater<int>())
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) (a * (b / gcd(a, b)))
+#define sq(x) (x) * (x)
+#define CEIL(a, b) ((a / b) + ((a % b) != 0))
+
+#define POSL(x, v) (lower_bound(x.begin(), x.end(), v) - x.begin())
+#define POSU(x, v) (upper_bound(x.begin(), x.end(), v) - x.begin())
+#define min3(a, b, c) min(a, min(b, c))
+#define min4(a, b, c, d) min(a, min(b, min(c, d)))
+#define max3(a, b, c) max(a, max(b, c))
+#define max4(a, b, c, d) max(a, max(b, max(c, d)))
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+#define pb push_back
+#define nl printf("\n")
+
+/** STL Functions     */
+// pbds
+#define fbo(ind) find_by_order(ind)
+#define ook(val) order_of_key(val)
+
+/**      Extra storing              */
+#define UNIQUE(v) (v).erase(unique(all(v)), (v).end())
+
+/**        End of Ex Storing                */
+
+/**------- Char Chk----------*/
+inline bool isLower(char ch) { return (ch >= 'a' && ch <= 'z'); }
+inline bool isUpper(char ch) { return (ch >= 'A' && ch <= 'Z'); }
+inline bool isDigit(char ch) { return (ch >= '0' && ch <= '9'); }
+inline bool isVowel(char ch)
+{
+    ch = tolower(ch);
+    return (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u');
+}
+/*----------------------Graph Moves----------------*/
+// const int fx[]={+1,-1,+0,+0};
+// const int fy[]={+0,+0,+1,-1};
+// const int fx[]={+0,+0,+1,-1,-1,+1,-1,+1};   // Kings Move
+// const int fy[]={-1,+1,+0,+0,+1,+1,-1,-1};  // Kings Move
+// const int fx[]={-2, -2, -1, -1,  1,  1,  2,  2};  // Knights Move
+// const int fy[]={-1,  1, -2,  2, -2,  2, -1,  1}; // Knights Move
+/*------------------------------------------------*/
+
+void solve()
+{
+    int n;
+    cin >> n;
+    string st1, st2;
+    cin >> st1 >> st2;
+    string res;
+    int ok = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (st1[i] == st2[i])
+        {
+            res.pb(st1[i]);
+        }
+        else
+        {
+            res.pb('9');
+        }
+    }
+
+    int h = n / 2;
+    if (n % 2 == 1)
+    {
+        if (st1[h] != st2[h])
+        {
+            cout << "-1\n";
+            return;
+        }
+    }
+
+    int ok = 0;
+    int ts1 = 0, ts2 = 0;
+    for (int i = 0; i < h; i++)
+    {
+        int i2 = n - i - 1;
+        if (res[i2] == '9' && res[i] == '9')
+        {
+            if (ok)
+            {
+                res[i] = res[i2];
+                ok = 1 - ok;
+            }
+            else
+            {
+                res[i2] = res[i];
+                ok = 1 - ok;
+            }
+        }
+        else if (res[i] == '9')
+        {
+            res[i] == res[i2];
+            if (res[i2] == st1[i2])
+            {
+                ts1++;
+            }
+            else
+                ts2++;
+        }
+        else if (res[i2] == '9')
+        {
+            res[i2] = res[i];
+            if (res[i] == st1[i])
+                ts1++;
+            else
+                ts2++;
+        }
+    }
+}
+
+int main()
+{
+    int tc = 1;
+    cin >> tc;
+    for (int i = 1; i <= tc; i++)
+    {
+        solve();
+    }
+
+    return 0;
+}
+
+/*"Success isn't permanent, failure isn't fatal,
+                                     it's the courage to continue that counts"*/
