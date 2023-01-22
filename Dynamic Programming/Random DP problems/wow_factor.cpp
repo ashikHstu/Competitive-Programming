9c51e89f445d1937fc267463a680a5d098e565c0
ashikur rahman <ashik.cse.hstu@gmail.com>
2023-01-23 00:03:48 +0600

    "beautiful DP"

---
+++ Dynamic Programming/Random DP problems/wow_factor.cpp
@@ -0,0 +1,148 @@
+/**        بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ (In the name of God, the Most Gracious, the Most Merciful.)       */
+/*
+      Institution : Hajee Mohammad Danesh Science and Technology University,Dinajpur,Bangladesh.
+      Name        : Ashikur Rahman Bhuyain (Asif)
+      Email       : ashik.cse.hstu@gmail.com
+*/
+
+/*
+   Problem link : https://codeforces.com/problemset/problem/1178/B
+   verdict : Accepted
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+
+#define FasterIO             \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0)
+#define TIME clock() * 1.0 / CLOCKS_PER_SEC
+#define pi acos(-1.0)
+#define mem(a, b) memset(a, b, sizeof(a))
+#define all(a) a.begin(), a.end()
+#define Sort(x) sort(x.begin(), x.end())
+#define Reverse(x) reverse(x.begin(), x.end())
+#define SortA(ar, s) sort(ar, ar + s)
+#define SortD(ar, s) sort(ar, ar + s, greater<int>())
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) (a * (b / gcd(a, b)))
+#define sq(x) (x) * (x)
+#define CEIL(a, b) ((a / b) + ((a % b) != 0))
+
+#define POSL(x, v) (lower_bound(x.begin(), x.end(), v) - x.begin())
+#define POSU(x, v) (upper_bound(x.begin(), x.end(), v) - x.begin())
+#define min3(a, b, c) min(a, min(b, c))
+#define min4(a, b, c, d) min(a, min(b, min(c, d)))
+#define max3(a, b, c) max(a, max(b, c))
+#define max4(a, b, c, d) max(a, max(b, max(c, d)))
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+#define pb push_back
+#define nl printf("\n")
+
+/** STL Functions     */
+// pbds
+#define fbo(ind) find_by_order(ind)
+#define ook(val) order_of_key(val)
+
+/**      Extra storing              */
+#define UNIQUE(v) (v).erase(unique(all(v)), (v).end())
+
+/**        End of Ex Storing                */
+
+/**------- Char Chk----------*/
+inline bool isLower(char ch) { return (ch >= 'a' && ch <= 'z'); }
+inline bool isUpper(char ch) { return (ch >= 'A' && ch <= 'Z'); }
+inline bool isDigit(char ch) { return (ch >= '0' && ch <= '9'); }
+inline bool isVowel(char ch)
+{
+    ch = tolower(ch);
+    return (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u');
+}
+/*----------------------Graph Moves----------------*/
+// const int fx[]={+1,-1,+0,+0};
+// const int fy[]={+0,+0,+1,-1};
+// const int fx[]={+0,+0,+1,-1,-1,+1,-1,+1};   // Kings Move
+// const int fy[]={-1,+1,+0,+0,+1,+1,-1,-1};  // Kings Move
+// const int fx[]={-2, -2, -1, -1,  1,  1,  2,  2};  // Knights Move
+// const int fy[]={-1,  1, -2,  2, -2,  2, -1,  1}; // Knights Move
+/*------------------------------------------------*/
+
+string str;
+int n;
+ll dp[1000006][5];
+ll findWay(int cur, int hoiche)
+{
+    if (hoiche == 3)
+        return 1;
+    if (cur == n)
+        return 0;
+    if (dp[cur][hoiche] != -1)
+        return dp[cur][hoiche];
+    ll Res = 0;
+    if (hoiche == 0 || hoiche == 2)
+    {
+        ll r1 = 0, r2 = 0;
+        if (str[cur] == 'w')
+        {
+            r1 = findWay(cur + 1, hoiche + 1);
+        }
+        r2 = findWay(cur + 1, hoiche);
+        Res = r1 + r2;
+    }
+    else
+    {
+        ll r1 = 0, r2 = 0;
+        if (str[cur] == 'o')
+        {
+            r1 = findWay(cur + 1, hoiche + 1);
+        }
+        r2 = findWay(cur + 1, hoiche);
+        Res = max(Res, (r1 + r2));
+    }
+    return dp[cur][hoiche] = Res;
+}
+
+void solve()
+{
+    mem(dp, -1);
+    string st;
+    cin >> st;
+    int wc = 0;
+    for (int i = 0; i < st.size(); i++)
+    {
+        if (st[i] == 'v')
+            wc++;
+        else
+        {
+            int tw = wc - 1;
+            for (int j = 0; j < tw; j++)
+                str.pb('w');
+            str.pb('o');
+            wc = 0;
+        }
+    }
+    for (int j = 1; j < wc; j++)
+        str.pb('w');
+    n = str.size();
+
+    ll res = findWay(0, 0);
+    cout << res << endl;
+}
+
+int main()
+{
+    int tc = 1;
+    // cin >> tc;
+    for (int i = 1; i <= tc; i++)
+    {
+        solve();
+    }
+
+    return 0;
+}
+
+/*"Success isn't permanent, failure isn't fatal,
+                                     it's the courage to continue that counts"*/
