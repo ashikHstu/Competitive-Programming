8808c2172a89ffcafe96d815bd5af64e44c0a4a2
ashikur rahman <ashik.cse.hstu@gmail.com>
2023-02-28 17:16:36 +0600

    "applying max to segment"

--- Data Structure/Segment Tree/segment Tree Part 2/Step 1/B_Applying_MAX_to_Segment.cpp
+++ Data Structure/Segment Tree/segment Tree Part 2/Step 1/B_Applying_MAX_to_Segment.cpp
@@ -4,123 +4,103 @@
       Name        : Ashikur Rahman Bhuyain (Asif)
       Email       : ashik.cse.hstu@gmail.com
 */
-
 /*
-   Problem link :
-   verdict :
-*/
+   Problem link : https://codeforces.com/edu/course/2/lesson/5/1/practice/contest/279634/problem/B
+   Status : Accepted
 
-#pragma GCC optimize("O3,unroll-loops")
-#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+*/
 
 #include <bits/stdc++.h>
 using namespace std;
-
-#define ll long long int
-
-#define FasterIO             \
-    ios::sync_with_stdio(0); \
-    cin.tie(0);              \
-    cout.tie(0)
-#define TIME clock() * 1.0 / CLOCKS_PER_SEC
-#define pi acos(-1.0)
-#define mem(a, b) memset(a, b, sizeof(a))
-#define all(a) a.begin(), a.end()
-#define Sort(x) sort(x.begin(), x.end())
-#define Reverse(x) reverse(x.begin(), x.end())
-#define SortA(ar, s) sort(ar, ar + s)
-#define SortD(ar, s) sort(ar, ar + s, greater<int>())
-#define gcd(a, b) __gcd(a, b)
-#define lcm(a, b) (a * (b / gcd(a, b)))
-#define sq(x) (x) * (x)
-#define CEIL(a, b) ((a / b) + ((a % b) != 0))
-
-#define POSL(x, v) (lower_bound(x.begin(), x.end(), v) - x.begin())
-#define POSU(x, v) (upper_bound(x.begin(), x.end(), v) - x.begin())
-#define min3(a, b, c) min(a, min(b, c))
-#define min4(a, b, c, d) min(a, min(b, min(c, d)))
-#define max3(a, b, c) max(a, max(b, c))
-#define max4(a, b, c, d) max(a, max(b, max(c, d)))
-#define ABS(x) ((x) < 0 ? -(x) : (x))
-#define pb push_back
-#define nl printf("\n")
-
-/** STL Functions     */
-// pbds
-#define fbo(ind) find_by_order(ind)
-#define ook(val) order_of_key(val)
-
-/**      Extra storing              */
-#define UNIQUE(v) (v).erase(unique(all(v)), (v).end())
-
-/**        End of Ex Storing                */
-
-/**------- Char Chk----------*/
-inline bool isLower(char ch) { return (ch >= 'a' && ch <= 'z'); }
-inline bool isUpper(char ch) { return (ch >= 'A' && ch <= 'Z'); }
-inline bool isDigit(char ch) { return (ch >= '0' && ch <= '9'); }
-inline bool isVowel(char ch)
+#define int long long
+struct segTree
 {
-    ch = tolower(ch);
-    return (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u');
-}
-string to_upper(string a)
-{
-    for (int i = 0; i < (int)a.size(); ++i)
-        if (a[i] >= 'a' && a[i] <= 'z')
-            a[i] -= 'a' - 'A';
-    return a;
-}
-string to_lower(string a)
-{
-    for (int i = 0; i < (int)a.size(); ++i)
-        if (a[i] >= 'A' && a[i] <= 'Z')
-            a[i] += 'a' - 'A';
-    return a;
-}
-bool prime(ll a)
-{
-    if (a == 1)
-        return 0;
-    for (int i = 2; i <= round(sqrt(a)); ++i)
-        if (a % i == 0)
-            return 0;
-    return 1;
-}
-/* PRINTS */
-template <class T>
-void print_v(vector<T> &v)
-{
-    cout << "{";
-    for (auto x : v)
-        cout << x << ",";
-    cout << "\b}";
-}
+    int size;
+    vector<long long> operations;
 
-/*----------------------Graph Moves----------------*/
-// const int fx[]={+1,-1,+0,+0};
-// const int fy[]={+0,+0,+1,-1};
-// const int fx[]={+0,+0,+1,-1,-1,+1,-1,+1};   // Kings Move
-// const int fy[]={-1,+1,+0,+0,+1,+1,-1,-1};  // Kings Move
-// const int fx[]={-2, -2, -1, -1,  1,  1,  2,  2};  // Knights Move
-// const int fy[]={-1,  1, -2,  2, -2,  2, -1,  1}; // Knights Move
-/*------------------------------------------------*/
-
-void solve()
-{
-}
-
-int main()
-{
-    int tc = 1;
-    cin >> tc;
-    for (int i = 1; i <= tc; i++)
+    long long operation(long long a, long long b)
     {
-        solve();
+
+        return max(a, b);
+    }
+
+    void init(int n)
+    {
+        size = 1;
+        while (size < n)
+            size *= 2;
+        operations.assign(2 * size, 0);
+    }
+
+    void Add(int l, int r, int v, int x, int lx, int rx)
+    {
+        if (l >= rx || r <= lx)
+            return;
+        if (lx >= l && rx <= r)
+        {
+            operations[x] = operation(operations[x], v);
+            return;
+        }
+        int mid = (lx + rx) / 2;
+        Add(l, r, v, x * 2 + 1, lx, mid);
+        Add(l, r, v, x * 2 + 2, mid, rx);
+    }
+
+    void Add(int l, int r, int v)
+    {
+        Add(l, r, v, 0, 0, size);
+    }
+
+    long long Get(int i, int x, int lx, int rx)
+    {
+        if (rx - lx == 1)
+        {
+            return operations[x];
+        }
+        int mid = (lx + rx) / 2;
+        long long res;
+        if (i < mid)
+        {
+            res = Get(i, 2 * x + 1, lx, mid);
+        }
+        else
+        {
+            res = Get(i, 2 * x + 2, mid, rx);
+        }
+        return operation(res, operations[x]);
+    }
+
+    long long Get(int i)
+    {
+        return Get(i, 0, 0, size);
+    }
+};
+
+int32_t main()
+{
+    int n, q;
+    cin >> n >> q;
+
+    segTree ST;
+    ST.init(n);
+
+    for (int i = 0; i < q; i++)
+    {
+        int type;
+        cin >> type;
+        if (type == 1)
+        {
+            int l, r, v;
+            cin >> l >> r >> v;
+            ST.Add(l, r, v);
+        }
+        else
+        {
+            int ind;
+            cin >> ind;
+            cout << ST.Get(ind) << endl;
+        }
     }
 
     return 0;
 }
-
-/*"Success isn't permanent, failure isn't fatal,
-                                     it's the courage to continue that counts"*/
