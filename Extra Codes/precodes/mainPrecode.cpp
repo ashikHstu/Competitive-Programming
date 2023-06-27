a0cc74e837d8148f54dd60ec73fb2b9cf308e870
ashikur rahman <ashik.cse.hstu@gmail.com>
2023-06-27 21:47:04 +0600

    "cf upsolving"

--- Extra Codes/precodes/mainPrecode.cpp
+++ Extra Codes/precodes/mainPrecode.cpp
@@ -138,11 +138,22 @@ void print_v(vector<T> &v)
 /*  ..................... Bit Manipulation...................................*/
 
 #define isOn(S, j) (S & (1 << j))
-#define setBit(S, j) (S |= (1 << j))
+
 #define clearBit(S, j) (S &= ~(1 << j)) // turn off j'th bit
 #define toggleBit(S, j) (S ^= (1 << j))
-#define lowBit(S) (S & (-S))
+#define lowBit(S) (S & (-S)) // if S==20, it will give 4 (16+4==20)
 #define setAll(S, n) (S = (1 << n) - 1)
+long long int highBit(long long v)
+{
+  if (v == 0)
+    return 0;
+  long long ct = 1;
+  while (ct * 2 <= v)
+  {
+    ct *= 2;
+  }
+  return ct; // if v==20, it will give 16 (16+4==20)
+}
 
 #define modulo(S, N) ((S) & (N - 1)) // returns S % N, where N is a power of 2
 #define isPowerOfTwo(S) (!(S & (S - 1)))
