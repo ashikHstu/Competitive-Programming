427195c7d0295657372e2262c9721196a1d901c5
ashikur rahman <ashik.cse.hstu@gmail.com>
2023-06-27 23:46:26 +0600

    "upsolving"

---
+++ random solve/B_Chess_Tournament.cpp
@@ -0,0 +1,185 @@
+/**        بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ (In the name of God, the Most Gracious, the Most Merciful.)       */
+/*
+      Institution : Hajee Mohammad Danesh Science and Technology University,Dinajpur,Bangladesh.
+      Name        : Ashikur Rahman Bhuyain (Asif)
+      Email       : ashik.cse.hstu@gmail.com
+*/
+
+/*
+   Problem link :
+   verdict :
+*/
+
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+
+#define FasterIO             \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0)
+#define TIME clock() * 1.0 / CLOCKS_PER_SEC
+#define pi acos(-1.0)
+#define mem(a, b) memset(a, b, sizeof(a))
+#define all(a) a.begin(), a.end()
+#define Sort(x) sort(x.begin(), x.end())
+#define Reverse(x) reverse(x.begin(), x.end())
+#define SortA(ar, s) sort(ar, ar + s)
+#define SortD(ar, s) sort(ar, ar + s, greater<int>())
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) (a * (b / gcd(a, b)))
+#define sq(x) (x) * (x)
+#define CEIL(a, b) ((a / b) + ((a % b) != 0))
+
+#define POSL(x, v) (lower_bound(x.begin(), x.end(), v) - x.begin())
+#define POSU(x, v) (upper_bound(x.begin(), x.end(), v) - x.begin())
+#define min3(a, b, c) min(a, min(b, c))
+#define min4(a, b, c, d) min(a, min(b, min(c, d)))
+#define max3(a, b, c) max(a, max(b, c))
+#define max4(a, b, c, d) max(a, max(b, max(c, d)))
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+#define pb push_back
+#define nl printf("\n")
+
+/** STL Functions     */
+// pbds
+#define fbo(ind) find_by_order(ind)
+#define ook(val) order_of_key(val)
+
+/**      Extra storing              */
+#define UNIQUE(v) (v).erase(unique(all(v)), (v).end())
+
+/**        End of Ex Storing                */
+
+/**------- Char Chk----------*/
+inline bool isLower(char ch) { return (ch >= 'a' && ch <= 'z'); }
+inline bool isUpper(char ch) { return (ch >= 'A' && ch <= 'Z'); }
+inline bool isDigit(char ch) { return (ch >= '0' && ch <= '9'); }
+inline bool isVowel(char ch)
+{
+    ch = tolower(ch);
+    return (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u');
+}
+string to_upper(string a)
+{
+    for (int i = 0; i < (int)a.size(); ++i)
+        if (a[i] >= 'a' && a[i] <= 'z')
+            a[i] -= 'a' - 'A';
+    return a;
+}
+string to_lower(string a)
+{
+    for (int i = 0; i < (int)a.size(); ++i)
+        if (a[i] >= 'A' && a[i] <= 'Z')
+            a[i] += 'a' - 'A';
+    return a;
+}
+bool prime(ll a)
+{
+    if (a == 1)
+        return 0;
+    for (int i = 2; i <= round(sqrt(a)); ++i)
+        if (a % i == 0)
+            return 0;
+    return 1;
+}
+/* PRINTS */
+template <class T>
+void print_v(vector<T> &v)
+{
+    cout << "{";
+    for (auto x : v)
+        cout << x << ",";
+    cout << "\b}";
+}
+
+/*----------------------Graph Moves----------------*/
+// const int fx[]={+1,-1,+0,+0};
+// const int fy[]={+0,+0,+1,-1};
+// const int fx[]={+0,+0,+1,-1,-1,+1,-1,+1};   // Kings Move
+// const int fy[]={-1,+1,+0,+0,+1,+1,-1,-1};  // Kings Move
+// const int fx[]={-2, -2, -1, -1,  1,  1,  2,  2};  // Knights Move
+// const int fy[]={-1,  1, -2,  2, -2,  2, -1,  1}; // Knights Move
+/*------------------------------------------------*/
+
+void solve()
+{
+    int n;
+    cin >> n;
+    string st;
+    cin >> st;
+    int zc = 0;
+    vector<int> tp;
+    for (int i = 0; i < n; i++)
+    {
+        if (st[i] == '2')
+        {
+            zc++;
+            tp.pb(i);
+        }
+    }
+    // cout << "zc : " << zc << endl;
+    if (zc < 3 && zc > 0)
+    {
+        cout << "NO\n";
+        return;
+    }
+    cout << "YES\n";
+    int res[n + 2][n + 2];
+    mem(res, 0);
+
+    if (zc > 0)
+    {
+
+        for (int i = 1; i < zc; i++)
+        {
+            res[tp[i - 1]][tp[i]] = 1;
+            res[tp[i]][tp[i - 1]] = 2;
+        }
+        // cout << "problem?\n";
+        //  cout << zc - 1 << endl;
+        // cout << "tp indexes : " << tp[zc - 1] << ", " << tp[0] << endl;
+
+        // return;
+
+        res[tp[zc - 1]][tp[0]] = 1;
+        res[tp[0]][tp[zc - 1]] = 2;
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i == j)
+                cout << "X";
+            else if (res[i][j] == 0)
+                cout << "=";
+            else if (res[i][j] == 1)
+                cout << "+";
+            else if (res[i][j] == 2)
+                cout << "-";
+            else
+                cout << res[i][j];
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int tc = 1;
+    cin >> tc;
+    for (int i = 1; i <= tc; i++)
+    {
+        solve();
+    }
+
+    return 0;
+}
+
+/*"Success isn't permanent, failure isn't fatal,
+                                     it's the courage to continue that counts"*/
