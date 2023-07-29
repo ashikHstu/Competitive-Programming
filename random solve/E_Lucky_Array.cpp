1a502666793b84c511c3b0f89c2766a3a8f91644
ashikur rahman <ashik.cse.hstu@gmail.com>
2023-07-29 17:14:00 +0600

    solve

---
+++ random solve/E_Lucky_Array.cpp
@@ -0,0 +1,191 @@
+/**        بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ (In the name of God, the Most Gracious, the Most Merciful.)       */
+/*
+      Institution : Hajee Mohammad Danesh Science and Technology University,Dinajpur,Bangladesh.
+      Name        : Ashikur Rahman Bhuyain (Asif)
+      Email       : ashik.cse.hstu@gmail.com
+*/
+
+/*
+   Problem link :
+   verdict :
+*/
+
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+
+#define FasterIO             \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0)
+#define doshomik(x) cout << fixed << setprecision(x)
+#define TIME clock() * 1.0 / CLOCKS_PER_SEC
+#define pi acos(-1.0)
+#define mem(a, b) memset(a, b, sizeof(a))
+#define all(a) a.begin(), a.end()
+#define Sort(x) sort(x.begin(), x.end())
+#define Reverse(x) reverse(x.begin(), x.end())
+#define SortA(ar, s) sort(ar, ar + s)
+#define SortD(ar, s) sort(ar, ar + s, greater<int>())
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) (a * (b / gcd(a, b)))
+#define sq(x) (x) * (x)
+#define CEIL(a, b) ((a / b) + ((a % b) != 0))
+
+#define POSL(x, v) (lower_bound(x.begin(), x.end(), v) - x.begin())
+#define POSU(x, v) (upper_bound(x.begin(), x.end(), v) - x.begin())
+#define min3(a, b, c) min(a, min(b, c))
+#define min4(a, b, c, d) min(a, min(b, min(c, d)))
+#define max3(a, b, c) max(a, max(b, c))
+#define max4(a, b, c, d) max(a, max(b, max(c, d)))
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+#define pb push_back
+#define nl printf("\n")
+// #define priority_queue <int, vector<int>, greater<int>> minHeap
+/** STL Functions     */
+// pbds
+#define fbo(ind) find_by_order(ind)
+#define ook(val) order_of_key(val)
+
+/**      Extra storing              */
+#define UNIQUE(v) (v).erase(unique(all(v)), (v).end())
+
+/**        End of Ex Storing                */
+
+/**------- Char Chk----------*/
+inline bool isLower(char ch) { return (ch >= 'a' && ch <= 'z'); }
+inline bool isUpper(char ch) { return (ch >= 'A' && ch <= 'Z'); }
+inline bool isDigit(char ch) { return (ch >= '0' && ch <= '9'); }
+inline bool isVowel(char ch)
+{
+    ch = tolower(ch);
+    return (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u');
+}
+string to_upper(string a)
+{
+    for (int i = 0; i < (int)a.size(); ++i)
+        if (a[i] >= 'a' && a[i] <= 'z')
+            a[i] -= 'a' - 'A';
+    return a;
+}
+string to_lower(string a)
+{
+    for (int i = 0; i < (int)a.size(); ++i)
+        if (a[i] >= 'A' && a[i] <= 'Z')
+            a[i] += 'a' - 'A';
+    return a;
+}
+bool prime(ll a)
+{
+    if (a == 1)
+        return 0;
+    for (int i = 2; i <= round(sqrt(a)); ++i)
+        if (a % i == 0)
+            return 0;
+    return 1;
+}
+/* PRINTS */
+template <class T>
+void print_v(vector<T> &v)
+{
+    cout << "{";
+    for (auto x : v)
+        cout << x << ",";
+    cout << "\b}";
+}
+
+/*----------------------Graph Moves----------------*/
+// const int fx[]={+1,-1,+0,+0};
+// const int fy[]={+0,+0,+1,-1};
+// const int fx[]={+0,+0,+1,-1,-1,+1,-1,+1};   // Kings Move
+// const int fy[]={-1,+1,+0,+0,+1,+1,-1,-1};  // Kings Move
+// const int fx[]={-2, -2, -1, -1,  1,  1,  2,  2};  // Knights Move
+// const int fy[]={-1,  1, -2,  2, -2,  2, -1,  1}; // Knights Move
+/*------------------------------------------------*/
+
+int isLucky[20000], fTree[20000004], n, m;
+
+void luckyCalculate(int cur)
+{
+    isLucky[cur] = 1;
+    if (cur <= 1000)
+    {
+        luckyCalculate(cur * 10 + 4);
+        luckyCalculate(cur * 10 + 7);
+    }
+}
+
+long long int Query(int ind)
+{
+    long long int sum = 0;
+    while (ind > 0)
+    {
+        sum += fTree[ind];
+        ind -= (ind & (-ind)); // (ind&(-ind)) will give the rightmost set bit
+    }
+    return sum;
+}
+
+void Update(int ind, int value)
+{
+    while (ind <= n)
+    {
+        fTree[ind] += value;
+        ind += (ind & (-ind));
+    }
+}
+
+void solve()
+{
+    cin >> n >> m;
+    int ar[n + 2];
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> ar[i];
+        if (isLucky[ar[i]])
+            Update(i, 1);
+    }
+    for (int i = 0; i < m; i++)
+    {
+        string st;
+        int l, r;
+        cin >> st >> l >> r;
+        if (st == "count")
+        {
+            cout << Query(r) - Query(l - 1) << endl;
+        }
+        else
+        {
+            int d;
+            cin >> d;
+            for (int i = l; i <= r; i++)
+            {
+                if (isLucky[ar[i]] == 1)
+                    Update(i, -1);
+                ar[i] += d;
+                if (isLucky[ar[i]])
+                    Update(i, 1);
+            }
+        }
+    }
+}
+
+int main()
+{
+    luckyCalculate(0);
+    int tc = 1;
+    // cin >> tc;
+    for (int i = 1; i <= tc; i++)
+    {
+        solve();
+    }
+
+    return 0;
+}
+
+/*"Success isn't permanent, failure isn't fatal,
+                                     it's the courage to continue that counts"*/
