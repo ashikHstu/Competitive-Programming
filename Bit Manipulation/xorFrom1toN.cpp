01645167381bd6ac82f12de162a79eef26719562
ashikur rahman <ashik.cse.hstu@gmail.com>
2023-10-03 09:50:39 +0600

    XorFrom 1 to n

---
+++ Bit Manipulation/xorFrom1toN.cpp
@@ -0,0 +1,11 @@
+long long xorFrom1toN(long long n)
+{
+    if (n % 4 == 0)
+        return n;
+    else if (n % 4 == 1)
+        return 1;
+    else if (n % 4 == 2)
+        return n + 1;
+    else if (n % 4 == 3)
+        return 0;
+}
