9e6e6abe20c883f5033f79dcdcae3379de953280
ashikur rahman <ashik.cse.hstu@gmail.com>
2024-03-14 01:33:05 +0600

    number and sum of number is divisible by k

---
+++ Dynamic Programming/Digit Dp/number_and_sumOfDigitsOfNumber_isDivisibleBy_k.cpp
@@ -0,0 +1,168 @@
+/**        بِسْمِ اللهِ الرَّحْمٰنِ الرَّحِيْمِ (In the name of God, the Most Gracious, the Most Merciful.)       */
+/*
+      Institution : Hajee Mohammad Danesh Science and Technology University,Dinajpur,Bangladesh.
+      Name        : Ashikur Rahman Bhuyain (Asif)
+      Email       : ashik.cse.hstu@gmail.com
+*/
+
+/*
+   Problem link :
+   verdict :
+*/
+
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+
+#define FasterIO             \
+    ios::sync_with_stdio(0); \
+    cin.tie(0);              \
+    cout.tie(0)
+#define doshomik(x) cout << fixed << setprecision(x)
+#define TIME clock() * 1.0 / CLOCKS_PER_SEC
+#define pi acos(-1.0)
+#define mem(a, b) memset(a, b, sizeof(a))
+#define all(a) a.begin(), a.end()
+#define Sort(x) sort(x.begin(), x.end())
+#define Reverse(x) reverse(x.begin(), x.end())
+#define SortA(ar, s) sort(ar, ar + s)
+#define SortD(ar, s) sort(ar, ar + s, greater<int>())
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) (a * (b / gcd(a, b)))
+#define sq(x) (x) * (x)
+#define CEIL(a, b) ((a / b) + ((a % b) != 0))
+
+#define POSL(x, v) (lower_bound(x.begin(), x.end(), v) - x.begin())
+#define POSU(x, v) (upper_bound(x.begin(), x.end(), v) - x.begin())
+#define min3(a, b, c) min(a, min(b, c))
+#define min4(a, b, c, d) min(a, min(b, min(c, d)))
+#define max3(a, b, c) max(a, max(b, c))
+#define max4(a, b, c, d) max(a, max(b, max(c, d)))
+#define ABS(x) ((x) < 0 ? -(x) : (x))
+#define pb push_back
+#define nl printf("\n")
+// #define priority_queue <int, vector<int>, greater<int>> minHeap
+/** STL Functions     */
+// pbds
+#define fbo(ind) find_by_order(ind)
+#define ook(val) order_of_key(val)
+
+/**      Extra storing              */
+#define UNIQUE(v) (v).erase(unique(all(v)), (v).end())
+
+/**        End of Ex Storing                */
+
+/**------- Char Chk----------*/
+inline bool isLower(char ch) { return (ch >= 'a' && ch <= 'z'); }
+inline bool isUpper(char ch) { return (ch >= 'A' && ch <= 'Z'); }
+inline bool isDigit(char ch) { return (ch >= '0' && ch <= '9'); }
+inline bool isVowel(char ch)
+{
+    ch = tolower(ch);
+    return (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u');
+}
+string to_upper(string a)
+{
+    for (int i = 0; i < (int)a.size(); ++i)
+        if (a[i] >= 'a' && a[i] <= 'z')
+            a[i] -= 'a' - 'A';
+    return a;
+}
+string to_lower(string a)
+{
+    for (int i = 0; i < (int)a.size(); ++i)
+        if (a[i] >= 'A' && a[i] <= 'Z')
+            a[i] += 'a' - 'A';
+    return a;
+}
+bool prime(ll a)
+{
+    if (a == 1)
+        return 0;
+    for (int i = 2; i <= round(sqrt(a)); ++i)
+        if (a % i == 0)
+            return 0;
+    return 1;
+}
+/* PRINTS */
+template <class T>
+void print_v(vector<T> &v)
+{
+    cout << "{";
+    for (auto x : v)
+        cout << x << ",";
+    cout << "\b}";
+}
+
+/*----------------------Graph Moves----------------*/
+// const int fx[]={+1,-1,+0,+0};
+// const int fy[]={+0,+0,+1,-1};
+// const int fx[]={+0,+0,+1,-1,-1,+1,-1,+1};   // Kings Move
+// const int fy[]={-1,+1,+0,+0,+1,+1,-1,-1};  // Kings Move
+// const int fx[]={-2, -2, -1, -1,  1,  1,  2,  2};  // Knights Move
+// const int fy[]={-1,  1, -2,  2, -2,  2, -1,  1}; // Knights Move
+/*------------------------------------------------*/
+#define minHeap priority_queue<int, vector<int>, greater<int>>
+int a, b, k;
+string n;
+int memo[11][2][91][91];
+
+int fun(int index, int last, int sum_mod, int digit_sum_mod)
+{
+    if (index == n.size())
+    {
+        // cout << "last values : " << sum_mod << " " << digit_sum_mod << endl;
+        if (sum_mod == 0 && digit_sum_mod == 0)
+        {
+            //  cout << "here?" << endl;
+            return 1;
+        }
+        return 0;
+    }
+    if (memo[index][last][sum_mod][digit_sum_mod] != -1)
+        return memo[index][last][sum_mod][digit_sum_mod];
+
+    int till = (last ? n[index] - '0' : 9);
+    int res = 0;
+    for (int i = 0; i <= till; i++)
+    {
+        res += (fun(index + 1, last && (i == till),
+                    (sum_mod * 10 + i) % k,
+                    (digit_sum_mod + i) % k));
+    }
+    return memo[index][last][sum_mod][digit_sum_mod] = res;
+}
+int findSolve(int _n)
+{
+    n = to_string(_n);
+    if (k > 90)
+        return 0;
+    memset(memo, -1, sizeof(memo));
+    return fun(0, true, 0, 0);
+}
+
+void solve(int kase)
+{
+    cin >> a >> b >> k;
+    cout << "Case " << kase << ": ";
+    cout << ((findSolve(b) - findSolve(a - 1))) << endl;
+}
+
+int main()
+{
+    int tc = 1;
+    cin >> tc;
+    for (int i = 1; i <= tc; i++)
+    {
+        solve(i);
+    }
+
+    return 0;
+}
+
+/*"Success isn't permanent, failure isn't fatal,
+                                     it's the courage to continue that counts"*/
