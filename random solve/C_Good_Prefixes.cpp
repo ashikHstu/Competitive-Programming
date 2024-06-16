a2d938c574d2e813b65106714c182ca1cabe616f
ashikur rahman <ashik.cse.hstu@gmail.com>
2024-06-16 14:58:33 +0600

    cp upsolve

--- random solve/C_Good_Prefixes.cpp
+++ random solve/C_Good_Prefixes.cpp
@@ -5,20 +5,44 @@ class helper
 {
 private:
     int n;
+    vector<int> vec;
 
 public:
-    void setN(int n)
+    helper(int n, vector<int> vec)
     {
         this->n = n;
+        this->vec = vec;
     }
-    void getN()
+    int giveResult()
     {
-        return this->n;
+        long long mx = 0;
+        long long sum = 0;
+        int res = 0;
+        for (int i = 0; i < n; i++)
+        {
+            sum += vec[i];
+
+            mx = max(mx, (long long)vec[i]);
+
+            if (mx + mx == sum)
+            {
+                res++;
+                // cout << "mx , sum : " << mx << ", " << sum << endl;
+            }
+        }
+        return res;
     }
 };
 
 void solve()
 {
+    int n;
+    cin >> n;
+    vector<int> vec(n);
+    for (int &v : vec)
+        cin >> v;
+    helper help(n, vec);
+    cout << help.giveResult() << endl;
 }
 
 int main()
