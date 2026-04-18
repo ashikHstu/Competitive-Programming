6a60a45eb186e21cac2ba992fa77a0dc319125e0
ashikHstu <ashik.cse.hstu@gmail.com>
2026-04-18 17:28:01 +0600

    problem 7 solved

---
+++ JavaScript 30 days challenge/02_Basic Array Transformation/03_2626. Array Reduce Transformation.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    return nums.reduce(fn,init);
+};
+
+
+//without arr.reduce
+
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    let sum=init;
+    for(let i=0;i<nums.length;i++)
+    {
+        sum=fn(sum,nums[i]);
+    }
+    return sum;
+};
