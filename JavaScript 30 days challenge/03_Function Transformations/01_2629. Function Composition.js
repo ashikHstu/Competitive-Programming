8be17a8e973f48ca8fcd0f6b3beb18b860a33849
ashikHstu <ashik.cse.hstu@gmail.com>
2026-04-19 03:15:16 +0600

    added function related

--- JavaScript 30 days challenge/03_Function Transformations/01_2629. Function Composition.js
+++ JavaScript 30 days challenge/03_Function Transformations/01_2629. Function Composition.js
@@ -9,8 +9,29 @@ var compose = function(functions) {
         for(let i=functions.length-1;i>=0;i--)
         {
             n=functions[i](n);
-        }A
+        }
+
         return n;
+    }
+};
+
+/**
+ * const fn = compose([x => x + 1, x => 2 * x])
+ * fn(4) // 9
+ */
+
+
+// Using reduceright
+/**
+ * @param {Function[]} functions
+ * @return {Function}
+ */
+var compose = function(functions) {
+    
+    return function(x) {
+        
+        
+        return functions.reduceRight((arr,fn)=> fn(arr),x);
     }
 };
 
