f84b4e625908e95dd1e85367e8485cc881bdf3bd
ashikHstu <ashik.cse.hstu@gmail.com>
2026-04-21 20:52:31 +0600

    30 days of JS

---
+++ JavaScript 30 days challenge/03_Function Transformations/04_2623. Memoize.js
@@ -0,0 +1,29 @@
+/**
+ * @param {Function} fn
+ * @return {Function}
+ */
+function memoize(fn) {
+    let map = new Map();
+    return function(...args) {
+        let key=JSON.stringify(args);
+        if(map.has(key))
+        {
+            return map.get(key);
+        }
+        let res=fn(...args);
+        map.set(key,res);
+        return res;
+    }
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
