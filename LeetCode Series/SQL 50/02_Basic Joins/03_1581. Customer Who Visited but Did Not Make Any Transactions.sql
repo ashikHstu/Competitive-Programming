2f783851e9a6e5cad7a70c55213255e69bb696ce
ashikHstu <ashik.cse.hstu@gmail.com>
2026-04-28 18:42:40 +0600

    sql

---
+++ LeetCode Series/SQL 50/02_Basic Joins/03_1581. Customer Who Visited but Did Not Make Any Transactions.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+SELECT Visits.customer_id, COUNT(*) as count_no_trans
+FROM Visits LEFT JOIN Transactions
+ON Visits.visit_id = Transactions.visit_id
+WHERE Transactions.transaction_id IS NULL
+GROUP BY Visits.customer_id;
