3e555cba90d5e44a5f77cc11743bea83dab0d2d0
ashikHstu <ashik.cse.hstu@gmail.com>
2026-04-28 21:48:25 +0600

    sql 1661

---
+++ LeetCode Series/SQL 50/02_Basic Joins/05_1661. Average Time of Process per Machine.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+SELECT machine_id, 
+ROUND(
+    SUM(
+        CASE 
+           WHEN activity_type='end' THEN timestamp 
+           ELSE -timestamp
+        END
+    )/COUNT(DISTINCT process_id)
+,3) AS processing_time
+FROM Activity
+GROUP BY machine_id;
